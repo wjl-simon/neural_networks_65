3c4e328592c1ab2a0440279db0ba973a219626b4
Wenjun Li <simon30302@hotmail.com>
2020-02-28 18:21:31 +0000

    part3

--- part3_pricing_model_linear.py
+++ part3_pricing_model_linear.py
@@ -3,6 +3,15 @@ from sklearn.model_selection import train_test_split
 import pickle
 import numpy as np
 
+from part2_claim_classifier.py import ClaimClassifier
+
+from sklearn import preprocessing
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
 
 def fit_and_calibrate_classifier(classifier, X, y):
     # DO NOT ALTER THIS FUNCTION
@@ -44,7 +53,7 @@ class PricingModelLinear():
 
 
     # YOU ARE ALLOWED TO ADD MORE ARGUMENTS AS NECESSARY TO THE _preprocessor METHOD
-    def _preprocessor(self, X_raw):
+    def _preprocessor(self, X_raw,heading):
         """Data preprocessing function.
 
         This function prepares the features of the data for training,
@@ -54,6 +63,9 @@ class PricingModelLinear():
         ----------
         X_raw : ndarray
             An array, this is the raw data as downloaded
+        
+        heading: python list
+            a list of names of the features in the data set
 
         Returns
         -------
@@ -61,9 +73,72 @@ class PricingModelLinear():
             A clean data set that is used for training and prediction.
         """
         # =============================================================
-        # YOUR CODE HERE
 
-        return  # YOUR CLEAN DATA AS A NUMPY ARRAY
+        # convert the x_raw as pandas dataframe
+        dat = pd.DataFrame(X_raw)
+        dat.columns = heading
+
+        # dat.columns = ['id_policy', 'pol_bonus', 'pol_coverage', 'pol_duration', \
+        #     'pol_sit_duration', 'pol_pay_freq','pol_payd' , 'pol_usage', \
+        #         'pol_insee_code', 'drv_drv2', 'drv_age1', 'drv_age2', 'drv_sex1',\
+        #             'drv_sex2', 'drv_age_lic1', 'drv_age_lic2', 'vh_age', 'vh_cyl', \
+        #                 'vh_din','vh_fuel', 'vh_make','vh_model', 'vh_sale_begin', \
+        #                     'vh_sale_end', 'vh_speed', 'vh_type', 'vh_value', \
+        #                         'vh_weight', 'town_mean_altitude', \
+        #                             'town_surface_area', 'population', 'commune_code', \
+        #                                 'canton_code','city_district_code', \
+        #                                     'regional_department_code']
+
+
+        # drop the useless features
+        dat.drop(columns=["id_policy"],axis=1)
+        
+        # turn the YES/NO into binary
+        dat.pol_payd.map(dict(Yes=1, No=0))
+        dat.drv_drv2.map(dict(Yes=1, No=0))
+
+        ##############################################################
+        # Handeling features with non-numeric values
+        ##############################################################
+        
+        non_num_feature_names = ["pol_coverage","pol_pay_freq", \
+            "pol_usage","drv_sex1","drv_sex2","vh_fuel","vh_make",\
+                "vh_model","vh_type"]
+        # non-numeric features
+        non_num_feature = dat.drop(columns=non_num_feature_names,axis=1)
+
+        # label binarizer
+        lb = preprocessing.LabelBinarizer()
+        # each element is a one-hot-vectorised feature
+        vector_set = [] 
+        for i in range(len(non_num_feature_names)):
+            # select a feature (a column)
+            data = non_num_feature.loc[:,non_num_feature_names[i]]
+            # binarise: each element has N rows, each row is a one-hot 
+            # vector, where N is the num of data points
+            vectors = lb.fit_transform(data.to_numpy())
+            vector_set.append(vectors)
+
+        ##############################################################
+        # Handeling (normalise) features with non-numeric values
+        ##############################################################
+
+        data = dat.to_numpy()
+        # scaler/normaliser
+        normaliser = preprocessing.MinMaxScaler()
+        # clean data
+        X = normaliser.fit_transform(data)
+        
+
+        ##############################################################
+        # Merge the processed features into a clean training set
+        ##############################################################
+        for feature in vector_set:
+            X.append(feature,axis=1)
+
+        return X
+
+
 
     def fit(self, X_raw, y_raw, claims_raw):
         """Classifier training function.
@@ -90,6 +165,9 @@ class PricingModelLinear():
         # =============================================================
         # REMEMBER TO A SIMILAR LINE TO THE FOLLOWING SOMEWHERE IN THE CODE
         X_clean = self._preprocessor(X_raw)
+
+
+
 
         # THE FOLLOWING GETS CALLED IF YOU WISH TO CALIBRATE YOUR PROBABILITES
         if self.calibrate:
